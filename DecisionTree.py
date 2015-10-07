7bf8df836b54ccc627918f44dee710d3304d1566
ilyssa widen <ilyssa@ilyssas-MacBook-Pro.local>
2015-10-06 22:07:04 -0700

    add code for decision tree

--- DecisionTree.py
+++ DecisionTree.py
@@ -10,32 +10,40 @@ import xlrd
 def entropy(histogram):
     total = float(sum(histogram.values()))
     if total == 0:
-    	return 0.0
-    return -sum((num / total) * math.log(num / total, 2)
-    			for num in histogram.values() if num > 0)
+        return 0.0
+    return -sum(num / total * math.log(num / total, 2) for num in
+                histogram.values() if num > 0)
 
-def attributes_tree(dict_list, target_attribute):
-    target_attribute_outputs = attribute_outputs(dict_list,
-            target_attribute)
-    attributes = dict_list[0].keys()
-    attributes.remove(target_attribute)
-    result_dict = {}
-    for attribute in attributes:
-        attribute_output_list = attribute_outputs(dict_list, attribute)
-        attribute_output_dict = {}
-        for attribute_output in attribute_output_list:
-            target_attribute_output_dict = {}
-            for target_attrbute_output in target_attribute_outputs:
-                count = len([d for d in dict_list if d[attribute]
-                            == attribute_output and d[target_attribute]
-                            == target_attrbute_output])
-                target_attribute_output_dict[target_attrbute_output] = count
-            attribute_output_dict[attribute_output] = target_attribute_output_dict
-        result_dict[attribute] = attribute_output_dict
-    return result_dict
+def decision_tree(filename, target_attribute):
+    data = excel_to_dictionary(filename)
+    attributes_infogain = attribute_infogain(data, target_attribute)
+    best_attributes = sorted(attributes_infogain.items(), key=lambda x: \
+                             x[1])
+    attributes = [a[0] for a in best_attributes]
+    return decision_tree_helper(data, target_attribute, attributes)
 
-def sort_by_entropy(dict_list,target_attribute):
-	tree = attributes_tree(dict_list,target_attribute)
+
+def decision_tree_helper(data, target_attribute, attributes):
+    target_attribute_values = [d[target_attribute] for d in data]
+    if target_attribute_values.count(target_attribute_values[0]) \
+        == len(target_attribute_values):
+        return target_attribute_values[0]
+    if not attributes:
+        d = Counter(target_attribute_values)
+        return d.most_common(1)[0][0]
+    else:
+        curr_attr = attributes.pop()
+        tree = {}
+        decision = {curr_attr: tree}
+        curr_attr_values = set(d[curr_attr] for d in data)
+        for value in curr_attr_values:
+            data_part = [d for d in data if d[curr_attr] == value]
+            if data_part:
+                tree[value] = decision_tree_helper(data_part,
+                        target_attribute, attributes)
+            else:
+                return target_attribute_values[0]
+    return decision
 
 
 def attribute_infogain(data, target_attribute):
@@ -48,12 +56,16 @@ def attribute_infogain(data, target_attribute):
 
     attribute_infogain = {}
     for attribute in attributes:
-    	total_information = 0.0
-    	for value in set(d[attribute] for d in data):
-    		histogram = Counter(d[target_attribute] for d in data if d[attribute] == value)
-    		total_information += sum(histogram.values()) * entropy(histogram)
-    	attribute_infogain[attribute] = information_before - total_information
+        total_information = 0.0
+        for value in set(d[attribute] for d in data):
+            histogram = Counter(d[target_attribute] for d in data
+                                if d[attribute] == value)
+            total_information += sum(histogram.values()) \
+                * entropy(histogram)
+        attribute_infogain[attribute] = information_before \
+            - total_information
     return attribute_infogain
+
 
 def excel_to_dictionary(filename):
     book = xlrd.open_workbook(filename)
@@ -62,25 +74,46 @@ def excel_to_dictionary(filename):
     dict_list = []
     for row_index in xrange(1, first_sheet.nrows):
         d = {}
-        for col_index in xrange(first_sheet.ncols):
+        for col_index in xrange(1, first_sheet.ncols):
             d[col_names[col_index]] = first_sheet.cell(row_index,
                     col_index).value
         dict_list.append(d)
     return dict_list
 
 
-def attribute_outputs(dict_list, attribute):
-    outputs = set()
-    for d in dict_list:
-        outputs.add(d[attribute])
-    return list(outputs)
+# taken from stackoverflow
 
+def pretty(d, indent=0):
+    for (key, value) in d.iteritems():
+        if isinstance(value, dict):
+            print '\t' * indent + '%30s: {\n' % str(key).upper()
+            pretty(value, indent + 1)
+            print '\t' * indent + ' ' * 32 + '} # end of %s #\n' \
+                % str(key).upper()
+        elif isinstance(value, list):
+            for val in value:
+                print '\t' * indent + '%30s: [\n' % str(key).upper()
+                pretty(val, indent + 1)
+                print '\t' * indent + ' ' * 32 + '] # end of %s #\n' \
+                    % str(key).upper()
+        else:
+            print '\t' * indent + '%30s: %s' % (str(key).upper(),
+                    str(value))
 
 
 class TestStringMethods(unittest.TestCase):
 
-    def entropy(self):
-        self.assertEquals(entropy([5.0, 9.0]), 0.9402859586706309)
+    def test_decision_tree_helper(self):
+        test1 = [{'A': '1', 'B': 'a'}, {'A': '1', 'B': 'b'}]
+        self.assertEquals(decision_tree_helper(test1, 'A', ['A']), '1')
+
+        test2 = [{'A': '1', 'B': 'a'}, {'A': '1', 'B': 'b'}, {'A': '2',
+                 'B': 'b'}]
+        self.assertEquals(decision_tree_helper(test2, 'A', []), '1')
+
+    def test_entropy(self):
+        self.assertEquals(entropy(Counter({'A': 5.0, 'B': 9.0})),
+                          0.9402859586706309)
 
     def target_attribute_outputs(self):
         dict_list = [{'a': '1'}, {'a': '2'}, {'a': '1'}]
@@ -98,11 +131,23 @@ class TestStringMethods(unittest.TestCase):
         self.assertEquals(result['c']['t']['1'], 2)
 
     def test_attribute_infogain(self):
-        dict_list = excel_to_dictionary("Commute.xlsx")
+        dict_list = excel_to_dictionary('Commute.xlsx')
         out = attribute_infogain(dict_list, 'Commute')
-        result = {}
+        result = {
+            'Accident': 6.454231881430971,
+            'Hour': 9.989838434720816,
+            'Stall': 3.23495093255735,
+            'Weather': 1.6993443792675045,
+            }
         self.assertEquals(out, result)
 
 
 if __name__ == '__main__':
-    unittest.main()
+
+    # unittest.main()
+
+    d = decision_tree('Tennis.xlsx', 'PlayTennis')
+
+    # print d
+
+    pretty(d)
