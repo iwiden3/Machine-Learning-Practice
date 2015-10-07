43891b1ab686502bdba796c155244ce7fc970a31
ilyssa widen <ilyssa@ilyssas-MacBook-Pro.local>
2015-10-06 19:48:56 -0700

    refactor code to get info gain working

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from collections import Counter
 import unittest
 import math
 import xlrd
 
 
-def entropy(input_list):
-    result = 0
-    s = sum(input_list)
-    for num in input_list:
-        n = num / s
-        result -= n * math.log(n, 2)
-    return result
-
+def entropy(histogram):
+    total = float(sum(histogram.values()))
+    if total == 0:
+    	return 0.0
+    return -sum((num / total) * math.log(num / total, 2)
+    			for num in histogram.values() if num > 0)
 
 def attributes_tree(dict_list, target_attribute):
     target_attribute_outputs = attribute_outputs(dict_list,
@@ -35,6 +34,26 @@ def attributes_tree(dict_list, target_attribute):
         result_dict[attribute] = attribute_output_dict
     return result_dict
 
+def sort_by_entropy(dict_list,target_attribute):
+	tree = attributes_tree(dict_list,target_attribute)
+
+
+def attribute_infogain(data, target_attribute):
+    target_attribute_values = set(d[target_attribute] for d in data)
+    attributes = data[0].keys()
+    attributes.remove(target_attribute)
+
+    histogram = Counter(d[target_attribute] for d in data)
+    information_before = len(data) * entropy(histogram)
+
+    attribute_infogain = {}
+    for attribute in attributes:
+    	total_information = 0.0
+    	for value in set(d[attribute] for d in data):
+    		histogram = Counter(d[target_attribute] for d in data if d[attribute] == value)
+    		total_information += sum(histogram.values()) * entropy(histogram)
+    	attribute_infogain[attribute] = information_before - total_information
+    return attribute_infogain
 
 def excel_to_dictionary(filename):
     book = xlrd.open_workbook(filename)
@@ -60,23 +79,29 @@ def attribute_outputs(dict_list, attribute):
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_entropy(self):
+    def entropy(self):
         self.assertEquals(entropy([5.0, 9.0]), 0.9402859586706309)
 
-    def test_target_attribute_outputs(self):
+    def target_attribute_outputs(self):
         dict_list = [{'a': '1'}, {'a': '2'}, {'a': '1'}]
         out = attribute_outputs(dict_list, 'a')
         self.assertEquals(len(out), 2)
         self.assertTrue('1' in out)
         self.assertTrue('2' in out)
 
-    def test_attribute_tree(self):
+    def attribute_attribute_infogain(self):
         dict_list = [{'a': '1', 'c': 't'}, {'a': '2', 'c': 'b'},
                      {'a': '1', 'c': 't'}]
         out = attributes_tree(dict_list, 'a')
         result = {'c': {'t': {'1': 2, '2': 0}, 'b': {'1': 0, '2': 1}}}
         self.assertEquals(out, result)
         self.assertEquals(result['c']['t']['1'], 2)
+
+    def test_attribute_infogain(self):
+        dict_list = excel_to_dictionary("Commute.xlsx")
+        out = attribute_infogain(dict_list, 'Commute')
+        result = {}
+        self.assertEquals(out, result)
 
 
 if __name__ == '__main__':
